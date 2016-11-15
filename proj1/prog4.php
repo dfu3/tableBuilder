ed7ccdc41be2c9625f9f9b66e70184a85f9863f4
Dante <dfu3@njit.edu>
2016-11-15 18:40:36 -0500

    prog 4 done

---
+++ proj1/prog4.php
@@ -0,0 +1,180 @@
+<?php
+
+//Singleton
+class ConnectDb 
+{
+  
+  private static $instance = null;
+  private $conn;
+  
+  private $host = 'sql1.njit.edu';
+  private $user = 'dfu3';
+  private $pass = 'maynard6';
+  private $db = 'dfu3';
+  
+  
+  private function __construct() //est. connection to the DB
+  {
+    $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+
+    if ($this->conn->connect_error)
+      {
+	die("Connection failed: " . $this->conn->connect_error);
+      }
+
+  }
+  
+  public static function getInstance()//get the persistent instance of the connection 
+  {
+    if(!self::$instance)
+      {
+	self::$instance = new ConnectDb();
+      }
+   
+    return self::$instance;
+  }
+  
+  public function getConnection()//create initial connection 
+  {
+    return $this->conn;
+  }
+}
+
+//Factory
+class Customer
+{
+  private $custName = "";
+  private $contFirst = "";
+  private $contLast = "";
+  private $credLimit = 0;
+
+  function __construct() {}
+
+  function setName($name) { $this->custName = $name; }
+  
+  function getName() { return $this->custName; }
+
+  function setFirst($first) { $this->custFirst = $first; }
+
+  function setLast($last) { $this->custLast = $last; }
+
+  function setLimit($limit) { $this->credLimit = $limit; }
+
+  function profile()
+  {
+    echo "<h3>CUSTOMER NAME: $this->custName</h3>
+         <b>CONTACT:         $this->custFirst $this->custLast </b><br>
+         <b>CREDIT LIMIT:    $this->credLimit </b><hr><br>";
+  }
+
+}
+
+class CustomerFactory //creates customers given the content
+{
+  public static function makeCustomer($customerName, $contactFName, $contactLName, $creditLimit)
+  {
+    $cust = new Customer();
+    $cust->setName($customerName);
+    $cust->setFirst($contactFName);
+    $cust->setLast($contactLName);
+    $cust->setLimit($creditLimit);
+   
+    return $cust;
+  }
+}
+
+class CustomerList 
+{
+  private $customers = array();
+  private $custCount = 0;
+  
+  public function __construct() {}
+  
+  public function getCustCount() 
+  {
+    return $this->custCount;
+  }
+  private function setCustCount($newCount) 
+  {
+    $this->custCount = $newCount;
+  }
+  public function getCust($numToGet) 
+  {
+    if ( (is_numeric($numToGet)) && ($numToGet <= $this->getCustCount() ) ) 
+      {
+	return $this->customers[$numToGet];
+      } 
+    else { return NULL; }
+  }
+  public function addCustomer(Customer $cust_in) 
+  {
+    $this->setCustCount($this->getCustCount() + 1);
+    $this->customers[$this->getCustCount()] = $cust_in;
+    return $this->getCustCount();
+  }
+}
+
+
+interface Advertise
+{
+  public function makeAd();
+}
+
+class Flyer implements Advertise
+{
+  public function makeAd()
+  {
+    echo "making small flyer ad<br>";
+  }
+}
+
+class Billboard implements Advertise
+{
+  public function makeAd()
+  {
+    echo "making large billboard ad<br>";
+  }
+}
+
+class Strategy
+{
+  public function makeAd($size)
+  {
+    if($size > 20 )
+      {
+	$ad = new Billboard();
+      }
+    else
+      {
+	$ad = new Flyer();
+      }
+
+    $ad->makeAd();
+
+  }
+
+}
+
+//-------
+
+$instance = ConnectDb::getInstance();
+$conn = $instance->getConnection();
+$q = "select customerName, contactFirstName, contactLastName, creditLimit from customers limit 100";
+
+$customers = new CustomerList();
+
+foreach($conn->query($q) as $row) 
+{
+  $temp = CustomerFactory::makeCustomer($row['customerName'], $row['contactFirstName'], $row['contactLastName'], $row['creditLimit'] );
+  $customers->addCustomer($temp);
+
+  $size = strlen($temp->getName());
+  $strat = new Strategy();
+  $name = $temp->getName();
+  echo "<b>$name:</b>";
+  $strat->makeAd($size);
+}
+
+$conn->close();
+
+?>
